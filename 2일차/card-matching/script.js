5eaed264f763aae674d1b7ea6555ecbca1903333
unknown <sowonswoni23@gmail.com>
2023-03-10 14:07:55 +0900

    'update'

--- 2일차/card-matching/script.js
+++ 2일차/card-matching/script.js
@@ -9,7 +9,7 @@
 3 - 1 기록 초기화 가능
 3 - 2 localStorage로 어거지로 서버의기능을 구현(한 듯?)
 4 처음 3초 동안 카운트다운이 됨
-
+5 뒤집는 애니메이션 추가
 */
 
 // 대충 모듈이나 변수 선언하는 곳
@@ -26,7 +26,7 @@ let history = localStorage.history ? JSON.parse(localStorage.history) : [];
 
 // 함수들 선언
 
-// setTimeout을 async function으로
+// async 작업이 여러개가 아니여서 효율성이 발현되지는 않지만 뭐...
 async function delay(s) {
   return new Promise((resolve) => setTimeout(resolve, s * 1000));
 }
@@ -61,36 +61,37 @@ async function start() {
 }
 
 // 게임이 끝난 후 기록과 초기화
-function end() {
+async function end() {
   isGaming = false;
   hoverAmount = 0;
   clock.operate();
   history.push(clock.cur);
   localStorage.history = JSON.stringify(history);
-  setTimeout(() => {
-    loadHistory();
-    cardData.forEach((card, i) => card.flipFront(i));
-    clock.reset();
-    if (confirm("게임 끝! 재시작하시려면 확인을 눌러주세요.")) start();
-  }, 500);
+  await delay(0.5);
+  loadHistory();
+  cardData.forEach((card, i) => card.flipFront(i));
+  clock.reset();
+  confirm("게임 끝! 재시작하시려면 확인을 눌러주세요.") && start(); // 문법 장난 헿
 }
 
 // 저장소에 저장된 기록을 페이지에 로드
 function loadHistory() {
   const historyDom = document.querySelector("#history");
-  while (historyDom.firstChild) historyDom.firstChild.remove();
+  historyDom.innerHTML = "";
+  // while (historyDom.firstChild) historyDom.firstChild.remove(); => 솔직히 누가 귀찮게 이렇게 함?
 
-  const historyNs = history
-    .slice(0, 25)
-    .sort((a, b) => (a > b ? 1 : -1))
-    .map((cur, i) => {
-      const newP2 = document.createElement("p");
-      newP2.textContent = `${i < 9 ? `0${i + 1}` : i + 1} | ${parseTime(
-        new Stopwatch(cur).result()
-      )}`;
-      return newP2;
-    });
-  historyDom.append(...historyNs);
+  historyDom.append(
+    ...history
+      .slice(0, 25)
+      .sort((a, b) => (a > b ? 1 : -1))
+      .map((cur, i) => {
+        const newP2 = document.createElement("p");
+        newP2.textContent = `${i < 9 ? `0${i + 1}` : i + 1} | ${parseTime(
+          new Stopwatch(cur).result()
+        )}`;
+        return newP2;
+      })
+  );
 }
 
 // stopwatch 모듈의 출력을 화면에 보기 편하게 파싱(이 맞나? ㅋㅋ)
@@ -98,8 +99,8 @@ function parseTime([, , s, ms]) {
   return `${s}.${ms}초`;
 }
 
-// stopwatch 돌리기 위한 loop (루프 자체를 모듈 static function으로 넣을 걸 그랬나)
-function loop() {
+// stopwatch 돌리기 위한 loop(루프 자체를 모듈 static function으로 넣을 걸 그랬나)
+(async function loop() {
   if (clock.result()[1] === "01") {
     isGaming = false;
     hoverAmount = 0;
@@ -107,54 +108,48 @@ function loop() {
     cardData.forEach((card, i) => card.flipFront(i));
     alert("1분동안 뭐했냐 ㅋㅋ");
   }
+
   document.querySelector("#showtime").innerHTML = parseTime(clock.result());
-  setTimeout(loop, 10);
-}
-loop();
+
+  await delay(0.01);
+  loop();
+})();
 
 // 이 프로그램의 핵심. 게임 중 카드를 클릭하면 이루어지는 모든 메커니즘
+
 // 함수 안에서 전역변수를 접근하는 게 맘에 안 들지만 귀찮아요
-function flipAction(n) {
+async function flipAction(n) {
   // 게임 중이 아니라면 동작 안 함
   if (!isGaming) return;
 
-  // 카드가 이미 뒤집혔거나 뒤집히는 중이라면 동작 안 함
-  // 0.5초 딜레이를 위한 장치
+  // 카드가 뒤집혔거나 뒤집히는 중이라면 동작 안 함. 0.5초 딜레이를 위한 장치
   if (cardData[n].isFlipped || isFlipping) return;
 
-  // 위에 다 통과하면 카드 뒤집기
-  cardData[n].flipBack(n); // DOM 조작
-  cardData[n].isFlipped = true; // 뭔지 알져?
+  // 위에 통과하면 카드 뒤집기
+  cardData[n].flipBack(n);
 
   // 메커니즘이 첫번째 누르고 두번째 누르면 그 두 개를 비교
-  // 따라서 first와 second에다가 현재 카드 번호를 대입
+  // 따라서 first나 second에다가 현재 카드 번호를 대입
   if (first === -1 && second === -1) first = n;
   else if (first !== -1 && second === -1) second = n;
 
-  // first와 second에 둘 다 값이 있다면 둘 다 뒤집혀 있다는 뜻
-  // 맞는지 확인하는 과정과 다시 뒤집는 과정
+  // first와 second에 둘 다 값이 있다면 맞는지 확인
   if (first !== -1 && second !== -1) {
     if (cardData[first].name === cardData[second].name) {
-      // 카드 두 개가 일치하면 암 것도 안 함
       // 대신 두 개를 뒤집는 순간이 모든 카드를 뒤집는 상태를 만든다면 게임 끝
-      cardData.every(({ isFlipped }) => isFlipped) && end(); // 문법 장난 헿
+      first = -1;
+      second = -1;
+      if (cardData.every(({ isFlipped }) => isFlipped)) await end();
     } else {
-      // async라서 first랑 second가 0.5초 사이에 바뀌어버리기 때문에 재할당
-      const f = first;
-      const s = second;
+      // 0.5초 다시 덮는 동안 동작 안 되게
       isFlipping = true;
-      cardData[first].isFlipped = false;
-      cardData[second].isFlipped = false;
-
-      delay(0.5).then(() => {
-        isFlipping = false;
-        cardData[f].flipFront(f);
-        cardData[s].flipFront(s);
-      });
+      await delay(0.5);
+      cardData[first].flipFront(first);
+      cardData[second].flipFront(second);
+      first = -1;
+      second = -1;
+      isFlipping = false;
     }
-    //  first와 second 초기화
-    first = -1;
-    second = -1;
   }
 }
 
@@ -184,6 +179,8 @@ class Card {
   get img() {
     return "./img/" + this.name + ".jpg";
   }
+
+  // 뒤집는 애니메이션. 카드가 나중에 섞이기 때문에 파라미터 받아야 함
 
   // 뒷면으로 뒤집고
   flipBack(n) {
@@ -271,19 +268,18 @@ cardData.forEach((_, i) => {
   getCardEl(i).append(...childs);
 });
 
+// hover 기능 추가. 뒤집힌 건 작동 안 하게 하려고 js로 구현
 document.querySelectorAll(".column").forEach((el, i) => {
   el.addEventListener("mouseover", () => {
-    if (cardData[i].isFlipped) return;
-    el.style.transform = `scale(1.0${hoverAmount})`;
-    el.style.transition = ".1s ease";
+    if (!cardData[i].isFlipped) {
+      el.style.transform = `scale(1.0${hoverAmount})`;
+    }
   });
-});
 
-document.querySelectorAll(".column").forEach((el) => {
   el.addEventListener("mouseout", () => {
     el.style.transform = "scale(1)";
-    el.style.transition = ".1s ease";
   });
 });
 
+// 기록 로드하기
 loadHistory();
